1d8ef13847f3343585f65bfee3453b6202b674d8
Hikaru Kondo <hikaru.kondo@digitalidentity.co.jp>
2026-06-23 10:46:11 +0900

    fix: add iOS Firebase options

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) return web;
-    throw UnsupportedError(
-      'DefaultFirebaseOptions are not supported for this platform.',
-    );
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
   }
 
   static const FirebaseOptions web = FirebaseOptions(
@@ -16,5 +23,14 @@ class DefaultFirebaseOptions {
     projectId:         'mogunchu-triplist',
     authDomain:        'mogunchu-triplist.firebaseapp.com',
     storageBucket:     'mogunchu-triplist.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey:            'AIzaSyBOGYVxs9Vj1eXUxdSftUIjtdCh7tg3Nw0',
+    appId:             '1:187561670094:ios:4dffc26ef5b8a24047a79f',
+    messagingSenderId: '187561670094',
+    projectId:         'mogunchu-triplist',
+    storageBucket:     'mogunchu-triplist.firebasestorage.app',
+    iosBundleId:       'com.sakura9625.divingprepapp',
   );
 }
